53a282d3341635539d635ca051d1e089613cbe1f
dyari <01di4r@gmail.com>
2025-04-07 13:46:26 +0300

    networking setup

--- Better-IMDB/Network/APIClient.swift
+++ Better-IMDB/Network/APIClient.swift
@@ -11,15 +11,6 @@ import RxCocoa
 
 class APIClient {
     private let baseURL = URL(string: TMDBAPI.baseURLString)!
-
-    func send<T: Codable>(apiRequest: APIRequest) -> Observable<T> {
-        let request = apiRequest.request(with: baseURL)
-        return URLSession.shared.rx.data(request: request)
-            .map { data in
-                try JSONDecoder().decode(T.self, from: data)
-            }
-            .observe(on: MainScheduler.asyncInstance)
-    }
     
     func discover<T: Codable>() -> Observable<T> {
         let request = DiscoverRequest().request(with: baseURL)
